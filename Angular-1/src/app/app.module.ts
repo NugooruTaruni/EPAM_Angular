1895d67d7a23fbdba35814a1750dc101c03e4d80
Taruni <taruninugooru29@gmail.com>
2023-09-03 14:08:01 +0530

    commit

---
+++ Angular-1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { CoursesComponent } from './courses/courses.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-1/src/app/interfaces/Course.ts
@@ -0,0 +1,7 @@
+export interface Course {
+    id: string
+    title: string
+    creationDate: Date
+    duration: Number
+    description: string
+}
